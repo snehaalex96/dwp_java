8deb43bcc78fd1d056bcf4d44d233a6dd55107f2
Sneha Grace Alex <snehaalexgrace@gmail.com>
2024-10-04 11:34:33 +0100

    Update TicketServiceImpl.java

--- cinema-tickets-main/cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ cinema-tickets-main/cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -3,7 +3,9 @@ package uk.gov.dwp.uc.pairtest;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest.Type;
 import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
+import thirdparty.paymentgateway.TicketPaymentService;
 import thirdparty.paymentgateway.TicketPaymentServiceImpl;
+import thirdparty.seatbooking.SeatReservationService;
 import thirdparty.seatbooking.SeatReservationServiceImpl;
 public class TicketServiceImpl implements TicketService {
    
@@ -11,78 +13,60 @@ public class TicketServiceImpl implements TicketService {
      * Should only have private methods other than the one below.
      */
 
-    public  void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests) throws InvalidPurchaseException {
-    	 try {
-	    	 int noOfTickets=0, noOfAdults=0, noOfChild=0,totalAmount=0;	
-	    	 if(checkAccountIdValid(accountId))
-	    	 {
-		    	 for (TicketTypeRequest i : ticketTypeRequests) {
-		            if(i.getTicketType()!= TicketTypeRequest.Type.INFANT ) {
-		    	 	noOfTickets=noOfTickets+i.getNoOfTickets();
-		            }
-		            if(i.getTicketType()== TicketTypeRequest.Type.ADULT ) {
-		            	noOfAdults=noOfAdults+i.getNoOfTickets();	
-		            }
-		            if(i.getTicketType()== TicketTypeRequest.Type.CHILD ) {
-		            	noOfChild=noOfChild+i.getNoOfTickets();
-		            }
-		    	 }
-		    	 System.out.println("No of tickets " + noOfTickets);	
-		    	 System.out.println("No of Child " + noOfChild);	
-		    	 System.out.println("No of Adults " + noOfAdults);
-		    	 if(noOfAdults!=0) {
-			    	 if(noOfTickets<=20) {
-			      	   totalAmount= calculateTicketAmount(noOfAdults,noOfChild) ;
-			      	   new TicketPaymentServiceImpl().makePayment(accountId, totalAmount);
-			      	   new SeatReservationServiceImpl().reserveSeat(accountId, noOfTickets);
-			         }
-			         else 
-			      	   System.out.println("Invalid purchase please purchase 20 tickets at a time");   
-		    	 }else
-		    		 System.out.println("Reservation and purchase Invalid - No Adult");  
-	    	 } 
-	    }
-    	 catch(InvalidPurchaseException e) {
-		      System.out.println("InvalidPurchaseException => " + e.getMessage());
-		  } 
+	private final TicketPaymentService ticketPaymentService;
+    private final SeatReservationService seatReservationService;
+
+    public TicketServiceImpl(TicketPaymentService paymentService, SeatReservationService reservationService) {
+        this.ticketPaymentService = paymentService;
+        this.seatReservationService = reservationService;
+    }    
+    @Override
+    public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests) {
+    	if (ticketTypeRequests == null || ticketTypeRequests.length == 0) {
+            throw new IllegalArgumentException("At least one ticket must be purchased.");
+        }
+    	// Validate accountId
+        if (accountId <= 0) {
+            throw new IllegalArgumentException("Invalid account ID");
+        }
+
+        int totalTickets = 0;
+        int adultTickets = 0;
+        int totalCost = 0;
+        int seatsToReserve = 0;
+
+        for (TicketTypeRequest request : ticketTypeRequests) {
+            int quantity = request.getNumberOfTickets();
+            totalTickets += quantity;
+
+            switch (request.getType()) {
+                case ADULT:
+                    totalCost += quantity * 25;
+                    seatsToReserve += quantity;
+                    adultTickets += quantity;
+                    break;
+                case CHILD:
+                    totalCost += quantity * 15;
+                    seatsToReserve += quantity;
+                    break;
+                case INFANT:
+                    // Infants don't cost and don't reserve seats
+                    break;
+            }
+        }
+
+        // Check if more than 25 tickets
+        if (totalTickets > 25) {
+            throw new IllegalArgumentException("Cannot purchase more than 25 tickets");
+        }
+
+        // Ensure there are adult tickets if infants or children are purchased
+        if (adultTickets == 0 && totalTickets > 0) {
+            throw new IllegalArgumentException("At least one adult ticket must be purchased");
+        }
+
+        // Process payment and reserve seats
+        ticketPaymentService.makePayment(accountId, totalCost);
+        seatReservationService.reserveSeat(accountId, seatsToReserve);
     }
-    
-   private boolean checkAccountIdValid(long accountId ) {
-	   boolean isValid= false;
-	   try {
-		     if(accountId >0)
-		     {
-		    	 isValid= true; 
-		     }
-		     else
-		     {
-		    	 isValid=false;
-		    	 System.out.println("Account invalid");
-		     }
-		    }
-
-	  catch (InvalidPurchaseException e) {
-		      System.out.println("InvalidPurchaseException => " + e.getMessage());
-		    } 
-      return isValid;
-   }
-	   
-	private int calculateTicketAmount(int noOfAdults, int noOfChild) {
-		int adultTicketAmount=20*noOfAdults;
-		int childTicketAmount=10*noOfChild;
-		int totalAmount=adultTicketAmount+childTicketAmount;
-		System.out.println("Total " +totalAmount + " pounds");
-		return totalAmount;
-		
-	}
-
-    // Main function used  for code development
-
-//    public static void main(String args[]) {
-//    	TicketServiceImpl ts= new TicketServiceImpl();
-//    	 TicketTypeRequest tr1 = new TicketTypeRequest(TicketTypeRequest.Type.ADULT,21);
-//    	 TicketTypeRequest tr2 = new TicketTypeRequest(TicketTypeRequest.Type.INFANT,0);
-//    	 TicketTypeRequest tr3 = new TicketTypeRequest(TicketTypeRequest.Type.CHILD,21);
-//    	 ts.purchaseTickets((long) 1,new TicketTypeRequest[] { tr1,tr2,tr3});
-//    }
 }
